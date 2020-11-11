eb499ae2e6662c82a713e1220c3449af9d1abe3a
Raju Melveetilpurayil <codename.raju@gmail.com>
2020-11-11 20:40:01 +0000

    [+] Initial commit

---
+++ src/DropDownColorChange/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DropDownColorChange.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:DropDownList runat="server" ID="ddlAvailability3" AutoPostBack="true"
+                OnSelectedIndexChanged="ddlAvailability_OnSelectedIndexChanged"
+                CssClass="dropdowns">
+                <asp:ListItem ID="LT3"></asp:ListItem>
+                <asp:ListItem ID="RT3"></asp:ListItem>
+            </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ src/DropDownColorChange/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropDownColorChange
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LT3.Value = "True";
+                RT3.Value = "False";
+                ddlAvailability3.SelectedIndex = 0;
+                ddlAvailability3.ApplyStyle(new Style() { ForeColor = Color.Green });
+            }
+            LT3.Attributes.CssStyle.Add("color", "green");
+            RT3.Attributes.CssStyle.Add("color", "red");
+        }
+
+        protected void ddlAvailability_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var value = ddlAvailability3.SelectedValue;
+            if (value.ToLower() == "true")
+            {
+                ddlAvailability3.ApplyStyle(new Style() { ForeColor = Color.Green });
+            }
+            else
+            {
+                ddlAvailability3.ApplyStyle(new Style() { ForeColor = Color.Red });
+            }
+        }
+    }
+}
